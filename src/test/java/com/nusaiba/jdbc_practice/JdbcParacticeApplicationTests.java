ccdc4cafacd705e22dce290e8bfce9122b133224
nusaiba <saranusaibanizam@gmail.com>
2023-10-03 16:49:30 +0600

    initial commit

---
+++ src/test/java/com/nusaiba/jdbc_practice/JdbcParacticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nusaiba.jdbc_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcParacticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
